8ba1ce66289008f24f0e85a9ad0866b6be9f0075
Evelyn Mannix <nathaniel.bloomfield@unimelb.edu.au>
2024-10-22 17:23:43 +1100

    have stuff working I think

--- copybackresults.bash
+++ copybackresults.bash
@@ -1,5 +1,5 @@
 
-rsync -avh nbloomfield@spartan.hpc.unimelb.edu.au:/data/cephfs/punim0980/tests/cider/checkpoints .
-rsync -avh nbloomfield@spartan.hpc.unimelb.edu.au:/data/cephfs/punim0980/tests/cider/save .
-rsync -avh nbloomfield@spartan.hpc.unimelb.edu.au:/data/cephfs/punim0980/tests/cider/logs .
+rsync -avh nbloomfield@spartan.hpc.unimelb.edu.au:/data/cephfs/punim0980/tests/PIPNet/runs ./runs
+
+
 
